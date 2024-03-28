4ca3252765019e7c1d7d39f011b0880bdd114196
Pavel Putin <pavelputin2003@yandex.ru>
2024-03-28 10:21:40 +0300

    chore: init java spring project

---
+++ src/main/java/ru/vsu/restobook_backend/RestobookBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.restobook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestobookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestobookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsu/restobook_backend/RestobookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.restobook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestobookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
