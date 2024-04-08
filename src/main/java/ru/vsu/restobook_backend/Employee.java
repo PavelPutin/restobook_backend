50db7522db8dcaee267cbf883f8bb9004c84e469
Noel Julmiste <noeljulmistefils@gmail.com>
2024-04-09 00:09:02 +0300

    task

---
+++ src/main/java/ru/vsu/restobook_backend/Employee.java
@@ -0,0 +1,30 @@
+package ru.vsu.restobook_backend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Employee")
+@Data
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String login;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String comment;
+    private boolean changedPass;
+    private Restaurant restaurant;
+
+    // Constructor, getters, setters, and other methods can be added here
+
+}
