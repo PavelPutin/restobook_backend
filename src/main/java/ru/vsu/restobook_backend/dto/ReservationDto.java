6ef7e2c3d459351bf8c92da2b2304c9a47afaef1
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-16 16:37:27 +0300

    feat: make reservation dto RESTAURANT-49

---
+++ src/main/java/ru/vsu/restobook_backend/dto/ReservationDto.java
@@ -0,0 +1,23 @@
+package ru.vsu.restobook_backend.dto;
+
+import ru.vsu.restobook_backend.model.ReservationState;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+public record ReservationDto(
+        Optional<Integer> id,
+        int personsNumber,
+        String clientPhoneNumber,
+        String clientName,
+        Instant startDateTime,
+        int durationIntervalMinutes,
+        String employeeFullName,
+        Instant creatingDateTime,
+        Optional<ReservationState> state,
+        Optional<String> comment,
+        Optional<Integer> restaurantId,
+        Optional<List<Integer>>tableIds
+) {
+}
---
+++ src/main/java/ru/vsu/restobook_backend/model/ReservationState.java
@@ -0,0 +1,5 @@
+package ru.vsu.restobook_backend.model;
+
+public enum ReservationState {
+    WAITING, OPEN, CLOSED
+}
