e15719d89a3ab14f71b29a964f140883c2ca0924
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-21 10:46:48 +0300

    feat: get logged user information

--- src/main/java/ru/vsu/restobook_backend/controller/DemoController.java
+++ src/main/java/ru/vsu/restobook_backend/controller/DemoController.java
@@ -5,20 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping()
 public class DemoController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('restobook_user', 'restobook_admin')")
-    public String hello() {
-        return "Hello from Spring boot keycloak";
+    public String hello(Principal principal) {
+        return "Hello, %s, from Spring boot keycloak".formatted(principal.getName());
     }
 
     @GetMapping("/hello-2")
     @PreAuthorize("hasRole('restobook_admin')")
-    public String hello2() {
-        return "Hello from Spring boot keycloak ADMIN";
+    public String hello2(Principal principal) {
+        return "Hello, %s, from Spring boot keycloak ADMIN".formatted(principal.getName());
     }
 
     @GetMapping("/health")
