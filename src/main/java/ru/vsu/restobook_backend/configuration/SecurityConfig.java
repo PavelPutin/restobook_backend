ed645c921395c490aec8ec27b3fa2b87654fc379
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-21 10:18:07 +0300

    feat: make roles working locally

--- src/main/java/ru/vsu/restobook_backend/configuration/SecurityConfig.java
+++ src/main/java/ru/vsu/restobook_backend/configuration/SecurityConfig.java
@@ -1,8 +1,10 @@
 package ru.vsu.restobook_backend.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +19,12 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -29,7 +36,7 @@ public class SecurityConfig {
                                 .requestMatchers("/health").permitAll()
                                 .anyRequest().authenticated());
         http
-                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
         http
                 .sessionManagement(management -> management.sessionCreationPolicy(STATELESS));
         System.out.println("CONFIGURED");
--- src/main/java/ru/vsu/restobook_backend/controller/DemoController.java
+++ src/main/java/ru/vsu/restobook_backend/controller/DemoController.java
@@ -1,5 +1,6 @@
 package ru.vsu.restobook_backend.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('restobook_user', 'restobook_admin')")
     public String hello() {
         return "Hello from Spring boot keycloak";
     }
 
     @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('restobook_admin')")
     public String hello2() {
         return "Hello from Spring boot keycloak ADMIN";
     }
