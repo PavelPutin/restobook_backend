946e277f6966cbcaf0368e22786906abf510d7fc
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-21 10:56:29 +0300

    feat: configure keycloak

---
+++ src/main/java/ru/vsu/restobook_backend/configuration/KeycloakConfig.java
@@ -0,0 +1,43 @@
+package ru.vsu.restobook_backend.configuration;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Value("${keycloak.url}")
+    private String keycloakUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.clientId}")
+    private String clientId;
+
+//    @Value("${keycloak.clientSecret}")
+//    private String clientSecret;
+
+    @Value("${keycloak.username}")
+    private String username;
+
+    @Value("${keycloak.password}")
+    private String password;
+
+    @Bean
+    Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(keycloakUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.PASSWORD)
+                .clientId(clientId)
+//                .clientSecret(clientSecret)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
