8f68b4aa50a7bec7e1b78af7ce1c9bf03937406a
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-24 17:41:12 +0300

    feat: make restaurant model jpa

--- src/main/java/ru/vsu/restobook_backend/model/Restaurant.java
+++ src/main/java/ru/vsu/restobook_backend/model/Restaurant.java
@@ -1,6 +1,6 @@
 package ru.vsu.restobook_backend.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,13 +11,21 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "rest_name")
     private String name;
+    @Column(name = "legal_entity_name")
     private String legalEntityName;
     private String inn;
+    @Column(name = "rest_comment")
     private String comment;
+    @OneToMany(mappedBy = "restaurant")
     private List<Employee> employees;
+    @OneToMany(mappedBy = "restaurant")
     private List<Table> tables;
+    @OneToMany(mappedBy = "restaurant")
     private List<Reservation> reservations;
 
     // Геттеры и сеттеры для всех полей
