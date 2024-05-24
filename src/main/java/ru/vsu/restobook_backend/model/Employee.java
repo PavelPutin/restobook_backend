1306725ee6dc3f2169daf6991b0f8a469ff589c1
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-24 17:44:09 +0300

    feat: make employee model jpa

--- src/main/java/ru/vsu/restobook_backend/model/Employee.java
+++ src/main/java/ru/vsu/restobook_backend/model/Employee.java
@@ -1,9 +1,10 @@
 package ru.vsu.restobook_backend.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "Employee")
+@Entity(name = "employee")
 @Data
 @NoArgsConstructor
 public class Employee {
@@ -11,14 +12,16 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String login;
     private String surname;
+    @Column(name = "employee_name")
     private String name;
     private String patronymic;
+    @Column(name = "employee_comment")
     private String comment;
+    @Column(name = "changed_pass")
     private boolean changedPass;
-
-    // Constructor, getters, setters, and other methods can be added here
-
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", referencedColumnName = "id")
+    private Restaurant restaurant;
 }
