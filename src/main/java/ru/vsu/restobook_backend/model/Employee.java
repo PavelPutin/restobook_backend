b73cf1945455c56870cd44769ebfc0fdb65d10f6
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-24 17:19:03 +0300

    fix: remove mock comments

--- src/main/java/ru/vsu/restobook_backend/model/Employee.java
+++ src/main/java/ru/vsu/restobook_backend/model/Employee.java
@@ -1,24 +1,24 @@
-//package ru.vsu.restobook_backend.model;
-//
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity(name = "Employee")
-//@Data
-//@NoArgsConstructor
-//public class Employee {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//    private String login;
-//    private String surname;
-//    private String name;
-//    private String patronymic;
-//    private String comment;
-//    private boolean changedPass;
-//
-//    // Constructor, getters, setters, and other methods can be added here
-//
-//}
+package ru.vsu.restobook_backend.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Employee")
+@Data
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String login;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String comment;
+    private boolean changedPass;
+
+    // Constructor, getters, setters, and other methods can be added here
+
+}
--- src/main/java/ru/vsu/restobook_backend/model/Reservation.java
+++ src/main/java/ru/vsu/restobook_backend/model/Reservation.java
@@ -1,32 +1,32 @@
-//package ru.vsu.restobook_backend.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import java.time.Instant;
-//import java.time.Duration;
-//import java.util.List;
-//
-//@Entity(name = "reservation")
-//@Data
-//@NoArgsConstructor
-//public class Reservation {
-//    private int id;
-//    private int personsNumber;
-//    private String clientPhoneNumber;
-//    private String clientName;
-//    private Instant startDateTime;
-//    private Duration duration;
-//    private String employeeFullName;
-//    private Instant creatingDateTime;
-//    private String state; // Необходимо уточнить название поля
-//    private String reservationComment;
-//    @ManyToOne
-//    @JoinColumn(name = "restaurant_ID")
-//    private Restaurant restaurant;
-//    private List<Table> tables;
-//
-//
-//}
+package ru.vsu.restobook_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.List;
+
+@Entity(name = "reservation")
+@Data
+@NoArgsConstructor
+public class Reservation {
+    private int id;
+    private int personsNumber;
+    private String clientPhoneNumber;
+    private String clientName;
+    private Instant startDateTime;
+    private Duration duration;
+    private String employeeFullName;
+    private Instant creatingDateTime;
+    private String state; // Необходимо уточнить название поля
+    private String reservationComment;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_ID")
+    private Restaurant restaurant;
+    private List<Table> tables;
+
+
+}
