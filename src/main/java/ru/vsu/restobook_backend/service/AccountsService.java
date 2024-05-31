a1f47ea0754ccf44eff0edca6d1043f0de7cc509
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-05-31 21:14:09 +0300

    fix: remove patronymic and comment ignoring im employee update method

--- src/main/java/ru/vsu/restobook_backend/service/AccountsService.java
+++ src/main/java/ru/vsu/restobook_backend/service/AccountsService.java
@@ -134,13 +134,10 @@ public class AccountsService {
         var employee = findByIdWithException(employeeId);
         employee.setName(employeeDto.name());
         employee.setSurname(employeeDto.surname());
-        if (employeeDto.patronymic().isPresent()) {
-            employee.setPatronymic(employeeDto.patronymic().get());
-        }
-        if (employeeDto.comment().isPresent()) {
-            employee.setComment(employeeDto.comment().get());
-        }
-        return employeesRepository.save(employee);
+        employee.setPatronymic(employeeDto.patronymic().orElse(null));
+        employee.setComment(employeeDto.comment().orElse(null));
+        employeesRepository.save(employee);
+        return employee;
     }
 
     public void deleteEmployee(int restaurantId, int employeeId, JwtAuthenticationToken principal) {
